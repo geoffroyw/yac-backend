1ca402ecf20528c8e6c787fa88c95da7226bdcf2
Geoffroy Weyer <webweyer@gmail.com>
2015-06-18 17:23:56 -0700

    schema updated after rerunning all migrations on local

--- db/schema.rb
+++ db/schema.rb
@@ -11,31 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606161410) do
+ActiveRecord::Schema.define(version: 20150604201830) do
 
-  create_table "addresses", force: true do |t|
-    t.string   "address",     null: false
-    t.string   "address2"
-    t.string   "postal_code", null: false
-    t.string   "city",        null: false
+  create_table "addresses", force: :cascade do |t|
+    t.string   "address",     limit: 255, null: false
+    t.string   "address2",    limit: 255
+    t.string   "postal_code", limit: 255, null: false
+    t.string   "city",        limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "country_id",  null: false
-    t.integer  "customer_id", null: false
+    t.integer  "country_id",  limit: 4,   null: false
+    t.integer  "customer_id", limit: 4,   null: false
   end
 
-  create_table "countries", force: true do |t|
-    t.string   "name",       null: false
-    t.string   "iso",        null: false
+  create_table "countries", force: :cascade do |t|
+    t.string   "name",       limit: 255, null: false
+    t.string   "iso",        limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "customers", force: true do |t|
-    t.string   "first_name", null: false
-    t.string   "last_name",  null: false
-    t.string   "email"
-    t.string   "phone"
+  create_table "customers", force: :cascade do |t|
+    t.string   "first_name", limit: 255, null: false
+    t.string   "last_name",  limit: 255, null: false
+    t.string   "email",      limit: 255
+    t.string   "phone",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
