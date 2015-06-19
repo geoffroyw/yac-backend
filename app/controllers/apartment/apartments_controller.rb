a046de146fd8207b9c99b456d99f9a0e39b460bf
Geoffroy Weyer <webweyer@gmail.com>
2015-06-19 12:50:35 -0700

    apartment/show

--- app/controllers/apartment/apartments_controller.rb
+++ app/controllers/apartment/apartments_controller.rb
@@ -5,6 +5,14 @@ class Apartment::ApartmentsController < ApplicationController
   def index
     apartments = Apartment::Apartment.all
     render json: apartments, each_serializer: Apartment::ApartmentSerializer
+  end
 
+  def show
+    apartment = Apartment::Apartment.find params[:id]
+    if stale?(apartment, last_modified: apartment.updated_at)
+      render json: apartment, serializer: Apartment::ApartmentSerializer
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: {error: 'Country does not exist'}, status: :not_found
   end
 end
--- spec/controllers/apartment/apartments_controller_spec.rb
+++ spec/controllers/apartment/apartments_controller_spec.rb
@@ -23,5 +23,36 @@ RSpec.describe Apartment::ApartmentsController, type: :controller do
     end
   end
 
+  describe '#show' do
+    let(:expected_apartment) { FactoryGirl.create :apartment }
+
+    context 'when the apartment is found' do
+      before :each do
+        get :show, {:id => expected_apartment.id}
+      end
+
+      it 'responds with 200' do
+        expect(response).to be_success
+      end
+
+      it 'returns the apartment in JSON' do
+        body = JSON.parse(response.body)
+        customer = body['apartment']
+
+        expect(customer['id']).to eq(expected_apartment.id)
+        expect(customer['name']).to eq(expected_apartment.name)
+        expect(customer['capacity']).to eq(expected_apartment.capacity)
+        expect(customer['description']).to eq(expected_apartment.description)
+      end
+    end
+
+    context 'when the apartment is not found' do
+      it 'responds with 404' do
+        get :show, {:id => 2}
+        expect(response).to be_not_found
+      end
+    end
+  end
+
 
 end
