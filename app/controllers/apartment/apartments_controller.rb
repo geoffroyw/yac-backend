bbf13471f9de21db05d0c61934cb92cefdf97cb1
Geoffroy Weyer <webweyer@gmail.com>
2015-07-08 20:53:58 +0200

    includes equipments in apartments finder

--- app/controllers/apartment/apartments_controller.rb
+++ app/controllers/apartment/apartments_controller.rb
@@ -3,12 +3,12 @@ class Apartment::ApartmentsController < ApplicationController
   include ActionController::Serialization
 
   def index
-    apartments = Apartment::Apartment.all
+    apartments = Apartment::Apartment.includes(:equipments).all
     render json: apartments, each_serializer: Apartment::ApartmentSerializer
   end
 
   def show
-    apartment = Apartment::Apartment.find params[:id]
+    apartment = Apartment::Apartment.includes(:equipments).find params[:id]
     if stale?(apartment, last_modified: apartment.updated_at)
       render json: apartment, serializer: Apartment::ApartmentSerializer
     end
